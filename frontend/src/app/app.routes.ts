a43577810c4530dcbf19ad80bd9d75b0fdc2dd83
Vicentetw <perrottavicente@gmail.com>
2026-07-14 11:23:44 -0300

    instalo Angular y ccreo estructura core features shared

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { MainLayoutComponent } from './core/layout/main-layout/main-layout.component';
+import { LoginComponent } from './features/login/login.component';
+import { MapaComponent } from './features/mapa/mapa.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      { path: '', redirectTo: 'mapa', pathMatch: 'full' },
+      { path: 'mapa', component: MapaComponent },
+      { path: 'login', component: LoginComponent }
+    ]
+  }
+];
